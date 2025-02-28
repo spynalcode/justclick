a53992ebc3f01cd2d3d63b40ac09b9b60fa95075
Benjamin Easter <benjeaster1@gmail.com>
2025-02-28 10:06:31 +0100

    more edit

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ let data = [
         name:'Linea Build',
         network:'mainnet',
         img: 'https://images.ctfassets.net/64upluvbiuck/3zG1POx5uegiZ8ELkQJq6n/f61f18f345400b4767536f949a96ce03/site-logo.svg',
-        website: 'https://layer3.xyz/linea-park'
+        website: ''
     }, 
     
     
@@ -27,20 +27,20 @@ let data = [
         network:'testnet',
         website: "https://mirror.xyz/zenithv1.eth/hd_O1Eu2-xQIvqm-ny8zId7Iq-_qWuZt37-YUJxndKI",
         name: 'Polymer Labs',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuOPB1z39RNkOwmdEpp_b60l0-EESQQEsW-jwng7QveR3rpx1Eoh0JrFXbNQ&s'
+        img: ''
     },
     
     {  
         network: 'mainnet',
         name:'Hyperlane protocol',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnOMtMsFeDiiiRcLNvFdcPUz6NwRP-gI69Mw&usqp=CAU',
+        img: '',
         website: 'https://twitter.com/its_airdrop/status/1768968212375654573'
     },
     
     {
         network:'testnet',
         name:'Parrallel finance',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnWrcrikOIhMGvsLSEKoZ2Chy8cDqO-ntvkBAA5uSfaMRSx9izB6roNdtQ2Q&s',
+        img: '',
         website:'https://www.coingecko.com/learn/parallel-network-airdrop-how-to-earn-parallel-points'
     }, 
     
@@ -56,7 +56,7 @@ let data = [
         network:'testnet',
         website: 'https://boxmining.com/mitosis-mito-token-airdrop-guide/',
         name:'Mitosis',
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqLH77SVzSiERSB14k5DdRTOsB3AfKzAAyqjnZsOotYlksoWdfbUpz361tmg&s',
+        img: '',
         
     },
 
@@ -71,7 +71,7 @@ let data = [
         network: 'testnet',
         website: 'https://twitter.com/its_airdrop/status/1772640533728379064',
         name: 'OG labs',
-        img: 'https://0g.ai/zg-logo.svg'
+        img: ''
     },
 
     {
@@ -84,7 +84,7 @@ let data = [
         network:'testnet',
         website:'https://www.inco.org/blogs/introducing-the-inco-gentry-testnet/',
         name:'inco',
-        img:'https://images.ctfassets.net/5uiczo0ea0k1/1Jm6h8Q8nWiJ4oUnfXxYKk/d4900a11c02448f9d3d6f75c800b198e/2024-03-01_16.44.57.jpg'
+        img:''
     },
 
     {
@@ -99,7 +99,7 @@ let data = [
         network:'testnet',
         website:'https://galxe.com/ten/campaign/GCaCBtUXyk',
         name: 'Ten',
-        img:'https://pbs.twimg.com/profile_banners/1441687917932384266/1707198058/1500x500'
+        img:''
     },
 
     {
@@ -113,21 +113,21 @@ let data = [
         network:'testnet',
         website:'https://twitter.com/its_airdrop/status/1775167522532397293',
         name:'prom',
-        img:'https://dashboard.prom.io/_next/static/media/logo.621630f1.png'
+        img:''
     },
 
     {
         network:'testnet',
         website:'https://twitter.com/myAlphaDrops/status/1770827650874929659',
         name:'redstone',
-        img:'https://cdn.galxe.com/galaxy/avatar/fbc46e41-bde4-45d0-b2a4-b88db5caf396.png?optimizer=image&width=200&quality=100'
+        img:''
     },
 
     {
         network:'testnet',
         website:'https://twitter.com/Danny_Crypton/status/1778780781864771989',
         name:'Layern',
-        img:'https://cdn.galxe.com/galaxy/55ccab891049479094035c113f2a5f34/.png_thumbnail.webp'
+        img:''
     },
 
     { 
@@ -141,14 +141,14 @@ let data = [
         network:'mint blockchain',
         webstite:'https://www.google.com',
         name:'mint BlockChain',
-        img:'https://pbs.twimg.com/profile_images/1783320986093588480/uhUB3xKT_400x400.jpg'
+        img:''
     },
 
     {
         network:'testnet',
         website:'https://app.galxe.com/quest/privasea/',
         name: 'Privasea',
-        img:'https://cdn.galxe.com/galaxy/89df8748825e4d0086db00b4d29759ae/.jpeg_thumbnail.webp'
+        img:''
     }
     
 
