c76d23d681937414c7eb51c7f5aa253180164a6d
Benjamin Easter <benjeaster1@gmail.com>
2025-02-28 10:09:56 +0100

    test

--- data.js
+++ data.js
@@ -2,30 +2,24 @@ let data = [
     {
         raised: '$65M',
         network:'mainnet',
-        website: 'https://x.com/0xHvdes/status/1764645731305140597',
+        // website: 'https://x.com/0xHvdes/status/1764645731305140597',
         name: 'EclipseFND',
         img: ''
     },
     
-    
-    {
-        name:'Linea Build',
-        network:'mainnet',
-        img: 'https://images.ctfassets.net/64upluvbiuck/3zG1POx5uegiZ8ELkQJq6n/f61f18f345400b4767536f949a96ce03/site-logo.svg',
-        website: ''
-    }, 
+
     
     
     {
         network:'testnet',
-        website:'https://x.com/myAlphaDrops/status/1771119669010711007',
+        // website:'https://x.com/myAlphaDrops/status/1771119669010711007',
         name:'Mind Network',
         img: '',
     }, 
     
     {
         network:'testnet',
-        website: "https://mirror.xyz/zenithv1.eth/hd_O1Eu2-xQIvqm-ny8zId7Iq-_qWuZt37-YUJxndKI",
+        // website: "https://mirror.xyz/zenithv1.eth/hd_O1Eu2-xQIvqm-ny8zId7Iq-_qWuZt37-YUJxndKI",
         name: 'Polymer Labs',
         img: ''
     },
@@ -34,27 +28,27 @@ let data = [
         network: 'mainnet',
         name:'Hyperlane protocol',
         img: '',
-        website: 'https://twitter.com/its_airdrop/status/1768968212375654573'
+        // website: 'https://twitter.com/its_airdrop/status/1768968212375654573'
     },
     
     {
         network:'testnet',
         name:'Parrallel finance',
         img: '',
-        website:'https://www.coingecko.com/learn/parallel-network-airdrop-how-to-earn-parallel-points'
+        // website:'https://www.coingecko.com/learn/parallel-network-airdrop-how-to-earn-parallel-points'
     }, 
     
     
     {
         network:'testnet',
         name:'Monad Network',
-        website: 'https://x.com/CryptoHindiO/status/1892915315710857222',
+        // website: 'https://x.com/CryptoHindiO/status/1892915315710857222',
         img: '',
     }, 
     
     {
         network:'testnet',
-        website: 'https://boxmining.com/mitosis-mito-token-airdrop-guide/',
+        // website: 'https://boxmining.com/mitosis-mito-token-airdrop-guide/',
         name:'Mitosis',
         img: '',
         
@@ -62,34 +56,34 @@ let data = [
 
     {
         network: 'testnet',
-        website:'https://twitter.com/Blum_OG/status/1772321357658996816',
+        // website:'https://twitter.com/Blum_OG/status/1772321357658996816',
         name:'Morph',
         img:''
     },
 
     {
         network: 'testnet',
-        website: 'https://twitter.com/its_airdrop/status/1772640533728379064',
+        // website: 'https://twitter.com/its_airdrop/status/1772640533728379064',
         name: 'OG labs',
         img: ''
     },
 
     {
         network:'network',
-        website: 'https://twitter.com/Blum_OG/status/1773057810068308239',
+        // website: 'https://twitter.com/Blum_OG/status/1773057810068308239',
         name: 'Expresso Sys',
         img: ''
     },
     {
         network:'testnet',
-        website:'https://www.inco.org/blogs/introducing-the-inco-gentry-testnet/',
+        // website:'https://www.inco.org/blogs/introducing-the-inco-gentry-testnet/',
         name:'inco',
         img:''
     },
 
     {
         network:'testnet',
-        website:'https://galxe.com/BounceBit/campaign/GCTRPtTxht?referral_code=GRFr2JXqo-m3UbTuJtJOJ-xqOELLQ1aZsqhEA8m8tV4wmoG',
+        // website:'https://galxe.com/BounceBit/campaign/GCTRPtTxht?referral_code=GRFr2JXqo-m3UbTuJtJOJ-xqOELLQ1aZsqhEA8m8tV4wmoG',
         name: 'BounceBit',
         img:''
     },
@@ -97,56 +91,56 @@ let data = [
 
     {
         network:'testnet',
-        website:'https://galxe.com/ten/campaign/GCaCBtUXyk',
+        // website:'https://galxe.com/ten/campaign/GCaCBtUXyk',
         name: 'Ten',
         img:''
     },
 
     {
         network:'testnet',
-        website:'https://galxe.com/Vanar/campaign/GC4cxt4Kih',
+        // website:'https://galxe.com/Vanar/campaign/GC4cxt4Kih',
         name:'vanar',
         img:''
     },
     
     {
         network:'testnet',
-        website:'https://twitter.com/its_airdrop/status/1775167522532397293',
+        // website:'https://twitter.com/its_airdrop/status/1775167522532397293',
         name:'prom',
         img:''
     },
 
     {
         network:'testnet',
-        website:'https://twitter.com/myAlphaDrops/status/1770827650874929659',
+        // website:'https://twitter.com/myAlphaDrops/status/1770827650874929659',
         name:'redstone',
         img:''
     },
 
     {
         network:'testnet',
-        website:'https://twitter.com/Danny_Crypton/status/1778780781864771989',
+        // website:'https://twitter.com/Danny_Crypton/status/1778780781864771989',
         name:'Layern',
         img:''
     },
 
     { 
         network:'testnet',
-        website:'https://mvp.synthr.io/',
+        // website:'https://mvp.synthr.io/',
         name:'Synthr',
         img:''
     },
 
     {
         network:'mint blockchain',
-        webstite:'https://www.google.com',
+        // webstite:'https://www.google.com',
         name:'mint BlockChain',
         img:''
     },
 
     {
         network:'testnet',
-        website:'https://app.galxe.com/quest/privasea/',
+        // website:'https://app.galxe.com/quest/privasea/',
         name: 'Privasea',
         img:''
     }
